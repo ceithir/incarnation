e3e1971edbb9fede43224535bd8190968dd80440
Éric <ceathramh@gmail.com>
2017-04-21 11:08:37 +0200

    Bootstrapping project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+const title = `Incarnation`;
+
+class App extends Component {
+  render() {
+    return (
+      <h1>{title}</h1>
+    );
+  }
+}
+
+export default App;
